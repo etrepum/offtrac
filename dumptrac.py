3406fe409d77340b8f8a4a86d0f4a2d825c56b90
Bob Ippolito <bob@redivi.com>
2010-10-28 19:10:21 +0800

    sync reports

--- dumptrac.py
+++ dumptrac.py
@@ -19,7 +19,7 @@ import itertools
 import simplejson as json
 
 
-TRAC_URL = 'https://trac.mochimedia.net/login/jsonrpc'
+TRAC_URL = 'https://trac.mochimedia.net'
 FIELDS = (
     'component',
     'priority',
@@ -29,7 +29,7 @@ FIELDS = (
     'version',
     'milestone',
 )
-DIRS = ('ticket', 'changelog') + tuple('field/' + f for f in FIELDS)
+DIRS = ('report', 'ticket', 'changelog') + tuple('field/' + f for f in FIELDS)
 VERSION = 1
 MIN_RECENT = "2000-01-01T00:00:00"
 
@@ -49,37 +49,6 @@ def get_http_connection(scheme, host):
 
 def auth_header(user, password):
     return ('Authorization', 'Basic ' + (user + ':' + password).encode('base64').strip())
-
-
-def raw_http_request(method, url, body=None, headers={}):
-    """Make a HTTP ``method`` request to ``url``
-    with optional body and headers.
-
-    Returns a dict with::
-
-        {
-            'status': int,
-            'reason': str,
-            'version': str,
-            'headers': dict, # keys are all lowercase
-            'raw_headers': [(key, value)],
-            'body': str,
-        }
-
-    """
-    scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
-    if query:
-        path = '%s?%s' % (path, query)
-    conn = get_http_connection(scheme, netloc)
-    conn.request(method, path, body, headers)
-    try:
-        response = conn.getresponse()
-        body = response.read()
-        headers = response.getheaders()
-    except:
-        conn.close()
-        raise
-    return body
 
 
 def jsondatetime(s):
@@ -126,11 +95,56 @@ class Trac(object):
     def __init__(self, user, password, url=TRAC_URL):
         self.url = url
         self.headers = dict([auth_header(user, password)])
-        self.headers['Content-Type'] = 'application/json'
+        self.conn = None
+
+    def http_request(self, method, path, body=None, headers={}):
+        """Make a HTTP ``method`` request to ``self.url + path``
+        with optional body and headers.
+
+        Returns a dict with::
+
+            {
+                'status': int,
+                'reason': str,
+                'version': str,
+                'headers': dict, # keys are all lowercase
+                'raw_headers': [(key, value)],
+                'body': str,
+            }
+
+        """
+        (scheme, netloc, path,
+         query, fragment) = urlparse.urlsplit(self.url + path)
+        if query:
+            path = '%s?%s' % (path, query)
+        if self.conn is None:
+            self.conn = get_http_connection(scheme, netloc)
+        self.conn.request(method, path, body, headers)
+        try:
+            response = self.conn.getresponse()
+            body = response.read()
+            headers = response.getheaders()
+        except:
+            self.conn.close()
+            self.conn = None
+            raise
+        return body
+
+    def http_get(self, path):
+        res = self.http_request('GET',
+                               path,
+                               '',
+                               self.headers)
+        return res
 
     def raw_call(self, method, params):
         body = json.dumps({'method': method, 'params': params})
-        res = raw_http_request('POST', self.url, body, self.headers)
+        headers = dict(self.headers)
+        headers['Content-Type'] = 'application/json'
+        res = self.http_request('POST',
+                               '/login/jsonrpc',
+                               body,
+                               headers)
         return json.loads(res)
 
     def call(self, method, params):
@@ -160,6 +174,14 @@ class Trac(object):
     def component_list(self):
         return self.call('ticket.component.getAll', [])
 
+    def report_list(self):
+        raw = self.http_get('/report?asc=1&format=tab')
+        for line in raw.decode('utf8').splitlines()[1:]:
+            report, delim, title = line.partition('\t')
+            if not delim:
+                continue
+            yield report, title
+
     def yield_components(self, components):
         return itertools.izip(
             components,
@@ -185,6 +207,11 @@ class Trac(object):
             tickets,
             self.imulticall([('ticket.changeLog', [ticket_id, 0])
                              for ticket_id in tickets]))
+
+    def yield_reports(self, reports):
+        for report_id, title in reports:
+            sql = self.http_get('/report?id=%s&format=sql' % (report_id,))
+            yield report_id, {'title': title, 'sql': sql.decode('utf8')}
 
 
 class DB(object):
@@ -253,6 +280,11 @@ class DB(object):
         return self.metadata['recent']
 
     def pull(self, t):
+        print 'syncing reports'
+        reports = t.report_list()
+        for report_id, info in t.yield_reports(reports):
+            safe_id = url_safe_id(report_id)
+            write_json(self.path_join('report', '%s.json' % (safe_id,)), info)
         for field_name in FIELDS:
             print 'syncing', field_name
             ## TODO: This is a bad sync, we do not
@@ -263,7 +295,6 @@ class DB(object):
                                           field_name,
                                           '%s.json' % (safe_id,)),
                            info)
-
         print 'fetching changed ticket ids since', self.recent
         recent_tickets = t.recent_tickets(self.recent)
         print 'fetching metadata for %d tickets' % (len(recent_tickets),)
